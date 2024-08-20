629af6ce912822528d7e5db63a4d981773a9ad3e
kuzpugonka <6599255@bk.ru>
2024-08-20 21:39:45 +0300

    add day3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const swiperThumb = new Swiper(".gift__swiper_thumb", {
-  spaceBetween: 16,
-  slidesPerView: 6,
+  spaceBetween: 12,
+  slidesPerView: 5,
   freeMode: true,
-  
+  breakpoints: {
+    320: {
+      slidesPerView: 5,
+      spaceBetween: 12,
+    },
+    1141: {
+      spaceBetween: 16,
+      slidesPerView: 6,
+    },
+  },
 });
 
 const swiperNain = new Swiper(".gift__swiper_card", {
